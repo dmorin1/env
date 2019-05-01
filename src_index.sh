582cd337dca69812ff1e2a8004058e4771a3fd5b
Dan Morin <dmorin@MarchNetworks.com>
2019-05-01 10:24:01 -0700

    adding useful script

---
+++ src_index.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+PRJ=$PWD
+echo $PRJ
+cd /
+find -L $PRJ -name '*.c' -o -name '*.h' -o -name '*.cpp' -o -name '*.hpp' -o -name '*.atc' -o -name '*.py'| sed -e 's/.*/\"&\"/' > $PRJ/cscope.files
+cd $PRJ
+
+if [ -f "cscope.in.out" ]; then
+	rm cscope.in.out
+fi
+
+if [ -f "cscope.out.in" ]; then
+rm cscope.out.in
+fi
+
+if [ -f "cscope.out" ]; then
+rm cscope.out
+fi
+
+if [ -f "cscope.po.out" ]; then
+rm cscope.po.out
+fi
+
+if [ -f "cscope.out.po" ]; then
+rm cscope.out.po
+fi
+
+echo $PWD
+cscope -b -q -i$PRJ/cscope.files
+ctags -R
+
+# EOF
